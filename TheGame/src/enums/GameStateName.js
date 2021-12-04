d2a79c8b1efb08809d7544e1fdc9ff7cc0d83a50
Ishpal Mattu <IshpalSMattu@hotmail.com>
2021-12-04 14:14:51 -0500

    Tetris setup

---
+++ TheGame/src/enums/GameStateName.js
@@ -0,0 +1,10 @@
+const GameStateName = {
+	GameOver: 'game-over',
+	Play: 'play',
+	Victory: 'victory',
+	TitleScreen: 'title',
+	Highscore: 'highscore',
+	Instructions: 'instruction'
+};
+
+export default GameStateName;
---
+++ TheGame/src/states/TitleScreenState.js
@@ -0,0 +1,23 @@
+import State from "../../lib/State.js";
+
+export default class TitleScreenState extends State {
+	constructor() {
+		super();
+	}
+
+	enter(parameters) { 
+		
+	}
+
+	exit() { 
+
+	}
+
+	update(dt) { 
+
+	}
+
+	render() { 
+		
+	}
+}
