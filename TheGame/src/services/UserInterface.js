61e51c56c71900beda1b77fcbc2f017775748e75
Ishpal Mattu <IshpalSMattu@hotmail.com>
2021-12-11 02:02:55 -0500

    shape display frame refactored
    hold shape and next shape frame display refactored

--- TheGame/src/services/UserInterface.js
+++ TheGame/src/services/UserInterface.js
@@ -16,10 +16,19 @@ export default class UserInterface {
         this.gameBoard = gameBoard;
 
         // Values taken from game frame using paint
-        this.frameNextX = 607;
-        this.frameNextY = 50;
-        this.frameNextHeight = 190 - this.frameNextY;       
-        this.frameNextWidth = 747 - this.frameNextX;
+
+        this.nextFrameY = 42;
+        this.nextFrameInnerY = 50;
+        this.holdFrameY = 250;
+
+        this.rightFrameInnerX = 607;
+        this.rightFrameTextHeight = 190 - this.nextFrameInnerY;       
+        this.rightFrameTextWidth = 747 - this.rightFrameInnerX;
+
+        this.rightFrameX = 597;
+        this.rightFrameWidth = 762 - this.rightFrameX;
+        this.rightFrameHeight = 202 - this.nextFrameY;
+
 
         // Values taken from game frame using paint
         this.leftTextX = 90;
@@ -29,19 +38,20 @@ export default class UserInterface {
         this.scoreY = 112;
         this.lineY = 409;
 
-        this.frameX = 15;
-        this.frameWidth = 150;
+        this.leftFrameX = 15;
+        this.leftFrameWidth = 150;
         this.frameTitleCanvasHeight = 41.5;
         
         this.scoreFrameY = 45;
         this.levelFrameY = 193;
         this.lineFrameY = 341;
 
-        this.sprites = Sprite.generateSpritesFromSpriteSheet(
-            images.get(ImageName.GameFrame),
-            canvas.width, 
-            canvas.height
-        );
+        this.sprites = UserInterface.generateSprite();
+
+        this.gameFrameSpriteIndex = 0;
+        this.shapeFrameSpriteIndex = 1;
+
+
     }
 
     update(){
@@ -50,16 +60,17 @@ export default class UserInterface {
 
 
     render(){
-        // Draw the game frame
-        this.sprites[0].render(0, 0);
+        // Draw the game frame, next shape frame & hold shape frame
+        this.sprites[this.gameFrameSpriteIndex].render(0, 0);
+        this.sprites[this.shapeFrameSpriteIndex].render(this.rightFrameX, this.nextFrameY);
+        this.sprites[this.shapeFrameSpriteIndex].render(this.rightFrameX, this.holdFrameY);
 
         // Display Next Shape
-        const nextShapeRenderX = this.frameNextX + (this.frameNextWidth - this.gameBoard.nextShape.dimensions.x * BLOCK_SIZE)/2
-        const nextShapeRenderY = this.frameNextY + (this.frameNextHeight - this.gameBoard.nextShape.dimensions.y * BLOCK_SIZE)/2
+        const nextShapeRenderX = this.rightFrameInnerX + (this.rightFrameTextWidth - this.gameBoard.nextShape.dimensions.x * BLOCK_SIZE)/2
+        const nextShapeRenderY = this.nextFrameInnerY + (this.rightFrameTextHeight - this.gameBoard.nextShape.dimensions.y * BLOCK_SIZE)/2
         this.gameBoard.nextShape.render(nextShapeRenderX + this.determineNextShapeXOfset(), nextShapeRenderY + this.determineNextShapeYOfset())
 
         context.save();
-        //context.font = "50px MinecraftEvenings";
         context.font = "40px Tetris2";
         context.textBaseline = 'middle';
         context.textAlign = "center";
@@ -71,17 +82,20 @@ export default class UserInterface {
 
         // Draw white rectangle in each info frame so we can write on top
         context.fillStyle = "#ffff";
-        context.fillRect(this.frameX, this.scoreFrameY, this.frameWidth, this.frameTitleCanvasHeight)
-        context.fillRect(this.frameX, this.levelFrameY, this.frameWidth, this.frameTitleCanvasHeight)
-        context.fillRect(this.frameX, this.lineFrameY, this.frameWidth, this.frameTitleCanvasHeight)
+        context.fillRect(this.leftFrameX, this.scoreFrameY, this.leftFrameWidth, this.frameTitleCanvasHeight)
+        context.fillRect(this.leftFrameX, this.levelFrameY, this.leftFrameWidth, this.frameTitleCanvasHeight)
+        context.fillRect(this.leftFrameX, this.lineFrameY, this.leftFrameWidth, this.frameTitleCanvasHeight)
 
         // Write the title for each info frame
         context.font = "30px LiquidItalic";
         context.fillStyle = "#6ec6e8";
-        context.fillText("Score", this.frameWidth/2 + this.frameX, this.frameTitleCanvasHeight/2 + this.scoreFrameY, this.frameWidth)
-        context.fillText("Level", this.frameWidth/2 + this.frameX, this.frameTitleCanvasHeight/2 + this.levelFrameY, this.frameWidth)
-        context.fillText("Lines", this.frameWidth/2 + this.frameX, this.frameTitleCanvasHeight/2 + this.lineFrameY, this.frameWidth)
-
+        context.fillText("Score", this.leftFrameWidth/2 + this.leftFrameX, this.frameTitleCanvasHeight/2 + this.scoreFrameY, this.leftFrameWidth)
+        context.fillText("Level", this.leftFrameWidth/2 + this.leftFrameX, this.frameTitleCanvasHeight/2 + this.levelFrameY, this.leftFrameWidth)
+        context.fillText("Lines", this.leftFrameWidth/2 + this.leftFrameX, this.frameTitleCanvasHeight/2 + this.lineFrameY, this.leftFrameWidth)
+        
+        context.font = "40px LiquidItalic";
+        context.fillText("Next", this.rightFrameWidth/2 + this.rightFrameX, this.rightFrameHeight + this.nextFrameY);
+        context.fillText("Hold", this.rightFrameWidth/2 + this.rightFrameX, this.rightFrameHeight + this.holdFrameY);
         context.restore();
 
         
@@ -102,6 +116,26 @@ export default class UserInterface {
             case(ShapeType.z ): return BLOCK_SIZE/2;
             default: return 0;
         }
+    }
+
+    static generateSprite(){
+        // Magic numbers here are of x/y positions of the game frame sprite using paint.
+        const nextFrameX = 597;
+        const nextFrameWidth = 762 - nextFrameX;
+        const nextFrameY = 42;
+        const nextFrameHeight = 202 - nextFrameY;
+
+        const gameFrameX = 0;
+        const gameFrameY = 0;
+        const gameFrameWidth = 589 - gameFrameX;
+        const gameFrameHeight = images.get(ImageName.GameFrame).height;
+
+        const sprites = [];
+
+        sprites.push(new Sprite(images.get(ImageName.GameFrame), gameFrameX, gameFrameY, gameFrameWidth, gameFrameHeight));
+        sprites.push(new Sprite(images.get(ImageName.GameFrame), nextFrameX, nextFrameY, nextFrameWidth, nextFrameHeight));
+    
+        return sprites;
     }
 
 }
